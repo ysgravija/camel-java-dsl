72b6df4f32b5ab735c4940f30287d4ca78d5f23e
yeesheng <tyeesheng@yahoo.com>
2018-07-12 05:35:04 +0800

    Remove logging again

--- src/main/java/com/swift/ipla/MainApp.java
+++ src/main/java/com/swift/ipla/MainApp.java
@@ -11,7 +11,6 @@ public class MainApp {
      * A main() so we can easily run these routing rules in our IDE
      */
     public static void main(String... args) throws Exception {
-	System.out.println("Starting main again...");
 	Main main = new Main();
         main.addRouteBuilder(new MyRouteBuilder());
         main.run(args);
